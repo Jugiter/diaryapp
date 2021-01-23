1c72bace751cb2d2ef6d703ef8430304a7f6bf8b
Jugiter <70154984+Jugiter@users.noreply.github.com>
2021-01-23 23:22:00 +0900

    For making Sullivan project 4th class content.

--- lib/main.dart
+++ lib/main.dart
@@ -37,20 +37,65 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: ListView.builder(
-          itemBuilder:(context, index){
-            return Container(height:80, child:
-            ListTile(
-              title: Text('제목 입력',
-              style: TextStyle(fontSize:17.0, fontWeight:FontWeight.bold)),
+            itemBuilder:(context, index){
+              return Container(height:80, child:
+              ListTile(
+                  title: Text('제목 입력',
+                      style: TextStyle(fontSize:17.0, fontWeight:FontWeight.bold)),
                   subtitle: Text('내용 입력',
                       style: TextStyle(fontSize:14.0)),
-              trailing: Icon(Icons.keyboard_arrow_right)
-            ));
-          }
+                  trailing: Icon(Icons.keyboard_arrow_right)
+              ));
+            }
         ),
         floatingActionButton:FloatingActionButton(
-        child: Icon(Icons.add, color:Colors.white)
-    )
+          child: Icon(Icons.add, color:Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailDiaryPage()),
+            );
+          },
+        )
+    );
+  }
+}
+
+class DetailDiaryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Diary',
+            style: TextStyle(
+              fontSize: 24.0,
+            )),
+      ),
+      body: Row(children: <Widget>[
+        Flexible(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '제목'
+                    ),
+                  )),
+              Container(
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '내용'
+                    ),
+                  ))
+            ],
+          ),
+        )]),
+      floatingActionButton:
+      FloatingActionButton(child: Icon(Icons.save, color: Colors.white,)),
     );
   }
 }
